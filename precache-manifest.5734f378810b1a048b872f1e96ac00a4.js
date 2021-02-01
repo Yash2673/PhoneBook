d40c5c1d85c8d8ad5cd929dbe5c6ceb109bb61c3
Yash <yashgupta2673@gmail.com>
2021-02-01 16:52:09 +0530

    Updates

---
+++ precache-manifest.5734f378810b1a048b872f1e96ac00a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4108cca9b829aa0a9af06a97b3d7c64e",
+    "url": "/PhoneBook/index.html"
+  },
+  {
+    "revision": "6e22370170ca78290c8b",
+    "url": "/PhoneBook/static/css/main.e8ade29e.chunk.css"
+  },
+  {
+    "revision": "af5134fbad328bffe137",
+    "url": "/PhoneBook/static/js/2.6f04abf5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PhoneBook/static/js/2.6f04abf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e22370170ca78290c8b",
+    "url": "/PhoneBook/static/js/main.bfc457fa.chunk.js"
+  },
+  {
+    "revision": "823697b663fe9f7b25ef",
+    "url": "/PhoneBook/static/js/runtime-main.b5df9609.js"
+  }
+]);
